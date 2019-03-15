74a74f0adf0ebd587b8b80ea313cedd657855db3
Glor <peter.troy.shitikov@gamil.com>
2019-03-15 18:32:41 +0300

    Добавьте файлы проекта.

---
+++ My_CMS_on_ASP_NET_Core_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_CMS_on_ASP_NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
